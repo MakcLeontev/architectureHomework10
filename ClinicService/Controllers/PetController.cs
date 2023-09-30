1d669d034c6db884dfbd42a4c2567574fdc10506
MakcLeontev <lmaks21@mail.ru>
2023-09-30 20:57:15 +0300

    Add project files.

---
+++ ClinicService/Controllers/PetController.cs
@@ -0,0 +1,62 @@
+﻿using ClinicService.Models.Requests;
+using ClinicService.Models;
+using ClinicService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicService.Controllers
+{
+    public class PetController: ControllerBase
+    {
+        private readonly IPetRepository _petRepository;
+
+        public PetController(IPetRepository petRepository)
+        {
+            _petRepository = petRepository;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] CreatePetRequest createRequest)
+        {
+            int res = _petRepository.Create(new Pet
+            {
+                ClientId = createRequest.ClientId,
+                Name = createRequest.Name,
+                Birthday = createRequest.Birthday,
+            });
+            return Ok(res);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] UpdatePetRequest updateRequest)
+        {
+            int res = _petRepository.Update(new Pet
+            {
+                PetId = updateRequest.PetId,
+                ClientId = updateRequest.ClientId,
+                Name = updateRequest.Name,
+                Birthday = updateRequest.Birthday,
+            });
+            return Ok(res);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] int PetId)
+        {
+            int res = _petRepository.Delete(PetId);
+            return Ok(res);
+        }
+
+        [HttpGet("get-all")]
+        public IActionResult GetAll()
+        {
+            return Ok(_petRepository.GetAll());
+        }
+
+
+        [HttpGet("get/{PetId}")]
+        public IActionResult GetById([FromRoute] int PetId)
+        {
+            return Ok(_petRepository.GetById(PetId));
+        }
+    }
+}
